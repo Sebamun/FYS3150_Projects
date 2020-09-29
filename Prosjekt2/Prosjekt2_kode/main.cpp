00b6824dccbd2e1abc7c5eefb9cfb7524ff3c208
Sebamun <SebastianAmundsen99@gmail.com>
2020-09-29 15:16:07 +0200

    la til plotting for iterasjoner og CPU

--- Prosjekt2/Prosjekt2_kode/main.cpp
+++ Prosjekt2/Prosjekt2_kode/main.cpp
@@ -1,19 +1,16 @@
 #include "prosjekt2.hpp"
-
 int main(int argc, char const *argv[]) {
-    int n = atoi(argv[1]);
-    cout << n << argv[2] << endl;
-    return 0;
-    string filename_1 = "Egenverdier1";
-    string filename_2 = "Egenverdier2";
+    string runtime = "Output";
+    string filename_1 = "Egenverdier";
+    string filename_2 = "Egenverdier_2";
     string filename_3 = "Egenvektorer";
     string filename_4 = "Egenvektorer_2";
+    double dim = atof(argv[1]);
     mat A;
     double max = 1;
     double MaxNonDiag = 5.0E-10;
     double eps = 1.0E-15;
     int p, q;
-    double dim = 1000;
     mat R = arma::zeros<mat>(dim, dim);
     for (int i = 0; i < dim; i++){
       R(i,i) = 1;
@@ -32,7 +29,10 @@ int main(int argc, char const *argv[]) {
     }
     check(dim, 1, A, filename_2, filename_4);
     end = clock();
-    cout << "Diagonalization took " << iterations << " iterations" << endl;
-    cout << scientific << "CPU time (sec) : " << ((double)end - (double)start) / CLOCKS_PER_SEC << endl;
+    double time = ((double)end - (double)start) / CLOCKS_PER_SEC;
+    ofstream ofile;
+    ofile.open(runtime);
+    ofile<<iterations<<setw(15)<<time<<endl;
+    ofile.close();
     return 0;
 }
--- Prosjekt2/Prosjekt2_kode/run.py
+++ Prosjekt2/Prosjekt2_kode/run.py
@@ -1,5 +1,24 @@
 import subprocess
-
-ns = [10, 50, 100]
+import matplotlib.pyplot as plt
+ns = [10, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300]
+iterations = []
+CPU_time = []
 for n in ns:
-    subprocess.run(['./Tridiag.x', str(n), 'hei'])
+    subprocess.run(['./Tridiag.x', str(n)])
+    infile = open("Output","r")
+    for line in infile:
+        p = line.split()
+        iterations.append(float(p[0]))
+        CPU_time.append(float(p[1]))
+
+#Plotting:
+plt.title('Number of iterations')
+plt.plot(ns, iterations,'bo')
+plt.xlabel('Dimension')
+plt.ylabel('Iterations')
+plt.show()
+plt.title('The CPU time')
+plt.plot(ns, CPU_time,'bo')
+plt.xlabel('Dimension')
+plt.ylabel('CPU time')
+plt.show()
