fe22d647d4dc4851c318ef7ea29103a6417c4280
Sebamun <SebastianAmundsen99@gmail.com>
2020-11-12 19:00:07 +0100

    Skrevet litt

--- Project_4/Project4_code/Plotte.py
+++ Project_4/Project4_code/Plotte.py
@@ -5,5 +5,5 @@ import pandas as pd
 # Skriver om til dataframe:
 df = pd.read_csv("Output.txt", delim_whitespace=True, \
 index_col=False, names=["T","E_tot","E_var","M_tot","M_var","M_abs"]) # Leser av tekstfilen.
-plt.plot(df['T'], df['E_var'])
+plt.plot(df['T'], df['M_abs'])
 plt.show()
