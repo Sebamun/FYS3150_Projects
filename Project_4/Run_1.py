167b7ddf843044e60dca25ed388465d3e2575a1f
marcusberget <marcusberget@gmail.com>
2020-11-19 10:29:59 +0100

    legger til prosjekt 4 til master

---
+++ Project_4/Run_1.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import subprocess
+# Initialverdier:
+num_spins = 2  # Antall spinn.
+mcs = 1000000  # Monte Carlo cycles.
+init_temp = 0.1
+final_temp = 2.6
+temp_step = 0.01
+proc = 2
+# Kjoorer programmet vårt:
+subprocess.run(['mpirun', '-n', str(proc), './code.x', 'Textfiles/Output_1.txt',
+                str(num_spins), str(mcs), str(init_temp), str(final_temp), str(temp_step)])
+# Skriver om til dataframe:
+df = pd.read_csv("Textfiles/Output_1.txt", delim_whitespace=True,
+                 names=["T", "E_tot", "E_var", "M_tot", "M_var", "M_abs"])  # Leser av tekstfilen.
+# Initialverdier for analytiske beregninger:
+k = 1.0  # Boltzmann konstant.
+T = 1.0  # Temperatur.
+beta = 1/(k*T)
+Z = 4*np.cosh(8*beta) + 12
+# Energi:
+# Finner den numeriske verdien for E_m.
+E_num = np.array(df.loc[df['T'] == T]['E_tot'])[0]
+# Den analytiske verdien for E_m.
+E_ana = -32*(np.sinh(8*beta)/Z) / num_spins / num_spins
+diff_E = np.abs(E_num-E_ana)
+# Magnetisering:
+M_num = np.array(df.loc[df['T'] == T]['M_tot'])[0]
+M_ana = 0
+diff_M = np.abs(M_num-M_ana)
+# Varmekapastitet:
+E_var = np.array(df.loc[df['T'] == T]['E_var'])[0]
+C_num = beta * E_var**2
+C_ana = beta * ((256*np.cosh(8*beta) / Z) -
+                (-32*(np.sinh(8*beta)) / Z)**2) / (num_spins**4)
+diff_C = np.abs(C_ana-C_num)
+#Susceptibilitet:
+M_var = np.array(df.loc[df['T'] == T]['M_var'])[0]
+Chi_num = beta * M_var**2
+# Faar feil svar når jeg tar per spinn her?
+Chi_ana = beta * (32*np.exp(8)+128) / Z
+diff_Chi = np.abs(Chi_num-Chi_ana)
+# Printing:
+print("The difference is {:.5f} for the mean energy." .format(diff_E))
+print("The difference is {:.5f} for the mean magnetization." .format(diff_M))
+print("The difference is {:.5f} for the heat capacity." .format(diff_C))
+print("The difference is {:.5f} for the susceptibility." .format(diff_Chi))
+# Plotting:
+plt.title('Energy')
+plt.plot(df['T'], df['E_tot'])
+plt.plot(T, E_ana, 'o', label=f'Analytical value at T={T}')
+plt.xlabel('T [K]')
+plt.ylabel('Joule/n^2 [J]')
+plt.legend()
+plt.show()
+plt.title('Magnetization')
+plt.plot(df['T'], df['M_tot'])
+plt.plot(T, M_ana, 'o', label=f'Analytical value at T={T}')
+plt.xlabel('T')
+plt.legend()
+plt.show()
+plt.title('Heat capacity')
+plt.plot(df['T'], beta * np.array(df['E_var'])**2)
+plt.plot(T, C_ana, 'o', label=f'Analytical value at T={T}')
+plt.xlabel('T')
+plt.legend()
+plt.show()
+plt.title('Susceptibility')
+plt.plot(df['T'], beta * np.array(df['M_var'])**2)
+plt.plot(T, Chi_ana, 'o', label=f'Analytical value at T={T}')
+plt.xlabel('T')
+plt.legend()
+plt.show()
---
+++ Project_4/Run_2.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import subprocess
+import os
+os.remove("E_values.txt")  # Removes the file (for reset purposes).
+# Initialverdier:
+num_spins = 20  # Antall spinn.
+mcs = np.linspace(0, 50000, 200)  # Monte Carlo cycles.
+init_temp = 0.95
+final_temp = 1.05
+temp_step = 0.05
+proc = 2  # Antall kjerner.
+T_1 = 1.0  # Temper
+a = open("E_values.txt", "a+")
+
+for i in range(len(mcs)):
+    # Kjoorer programmet vårt:
+    subprocess.run(['mpirun', '-n', str(proc), './code.x', 'Textfiles/Output_2.txt',
+                    str(num_spins), str(mcs[i]), str(init_temp), str(final_temp), str(temp_step)])
+    # Skriver om til dataframe:
+    df = pd.read_csv("Textfiles/Output_2.txt", delim_whitespace=True,
+                     names=["T", "E_tot", "E_var", "M_tot", "M_var", "M_abs"])  # Leser av tekstfilen.
+    # Skriver energiverdiene til en annen fil:
+    value = np.array(df.loc[df['T'] == T_1]['E_tot'])[0]
+    a.write(str(value))
+    a.write(' ')
+a.close()
+
+r = open("E_values.txt", "r")
+for line in r:
+    q = line.split()
+    E = np.array(q)
+    E = E.astype(np.float)
+plt.plot(mcs, E)
+plt.show()
